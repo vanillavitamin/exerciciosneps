c74be6be5dd93d6c02b9ec2903b29deae3b628e8
Ana Gabriella 🎸 <anagabriellaferreiraribeiro3@gmail.com>
2026-03-23 00:57:21 -0300

    Add files via upload

---
+++ premiodomilhao.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N, A;
+    cin >> N;
+    
+    long long total = 0;
+    int dias = 0;
+    
+    for (int i = 0; i < N; i++){
+        cin >> A;
+        total += A;
+        if(soma >= 1000000){
+            dias = i;
+            break;
+        }
+        
+    }
+    
+    cout << dias;
+    return 0;
+}
